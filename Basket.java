2f97d701a878de168aba6ba444f0cd48c2aa5f45
edson-1222 <edsonwillie12@gmail.com>
2025-10-21 16:20:42 +0800

    Add Basket class with product capacity

---
+++ Basket.java
@@ -0,0 +1,12 @@
+/**
+ * Represents a shopping basket with 15 product capacity.
+ */
+public class Basket extends Equipment {
+    
+    /**
+     * Constructor for Basket
+     */
+    public Basket() {
+        super("Basket", 15);
+    }
+}
