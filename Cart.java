d73516022cc18355ba101d6d2eaf84b101a9c6b6
edson-1222 <edsonwillie12@gmail.com>
2025-10-21 14:48:21 +0800

    Add Cart class with product capacity

---
+++ Cart.java
@@ -0,0 +1,12 @@
+/**
+ * Represents a shopping cart with 30 product capacity.
+ */
+public class Cart extends Equipment {
+    
+    /**
+     * Constructor for Cart
+     */
+    public Cart() {
+        super("Cart", 30);
+    }
+}
